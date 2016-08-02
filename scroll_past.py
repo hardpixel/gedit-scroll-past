c021ec1f9ab6c47031cdee1978f735c4d907a434
jonian <jonian@hardpixel.eu>
2016-08-02 16:10:42 +0300

    refactoring

--- scroll_past.py
+++ scroll_past.py
@@ -1,4 +1,10 @@
-from gi.repository import GObject, Gtk, Gdk, Gedit
+import gi
+
+gi.require_version('Gdk', '3.0')
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gedit', '3.0')
+
+from gi.repository import GObject, Gdk, Gtk, Gedit
 
 class ScrollPastAppActivatable(GObject.Object, Gedit.AppActivatable):
 
@@ -7,21 +13,21 @@ class ScrollPastAppActivatable(GObject.Object, Gedit.AppActivatable):
 	def __init__(self):
 		GObject.Object.__init__(self)
 
-		self.style_provider = Gtk.CssProvider()
-		self.current_screen = Gdk.Screen.get_default()
+		self._style_provider = Gtk.CssProvider()
+		self._current_screen = Gdk.Screen.get_default()
 
 	def do_activate(self):
 		css = b""".gedit-view { padding-bottom: 400px }"""
-		self.style_provider.load_from_data(css)
+		self._style_provider.load_from_data(css)
 
 		Gtk.StyleContext.add_provider_for_screen(
-			self.current_screen,
-			self.style_provider,
+			self._current_screen,
+			self._style_provider,
 			Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
 		)
 
 	def do_deactivate(self):
 		Gtk.StyleContext.remove_provider_for_screen(
-			self.current_screen,
-			self.style_provider
+			self._current_screen,
+			self._style_provider
 		)
